bf90f5023459912e0222863943e4e2de804d4106
Daniel Lucas <danomiterock@yahoo.com>
2021-03-27 09:26:48 -0700

    add db tables

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -4,7 +4,26 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    -- department name
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+)
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+)
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
 )
