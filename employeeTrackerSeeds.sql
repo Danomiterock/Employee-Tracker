82eac5651b8e706443bc02b49e63eb597e2fbb32
Daniel Lucas <danomiterock@yahoo.com>
2021-03-27 09:11:11 -0700

    create initial files

---
+++ employeeTrackerSeeds.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    
+)
